368722f31f7252737be1939ed498dc2d8c42c81a
matbarPL <mateusz.baryla.1996@gmail.com>
2025-01-07 23:05:08 +0100

    feat: add industries and geographies in expertise section

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -4,6 +4,33 @@ import { srConfig } from '@config';
 import sr from '@utils/sr';
 import { usePrefersReducedMotion } from '@hooks';
 
+const StyledIndustries = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 10px 0;
+
+    li {
+      margin-bottom: 10px;
+      font-family: var(--font-mono);
+      font-size: var(--fz-xs);
+      position: relative;
+
+      &:before {
+        content: '▹';
+        position: absolute;
+        left: -15px;
+        color: var(--green);
+        font-size: var(--fz-sm);
+      }
+    }
+  }
+`;
+
 const StyledAboutSection = styled.section`
   max-width: 900px;
 
@@ -45,7 +72,6 @@ const StyledText = styled.div`
     }
   }
 `;
-
 const About = () => {
   const revealContainer = useRef(null);
   const prefersReducedMotion = usePrefersReducedMotion();
@@ -65,25 +91,20 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              I am your partner in harnessing the power of data. I believe that data is more than
-              just numbers; it's a valuable resource that can drive meaningful impact. I am
-              dedicated to helping you uncover hidden insights from your data and translate them
-              into actionable strategies.
-            </p>
-            <p>
-              I offer custom software solutions tailored to your specific data needs, data strategy
-              development, and data processing expertise.
+              I am your partner in harnessing the power of data. By treating data as a valuable
+              resource rather than just numbers, opportunities are unlocked to make informed
+              decisions and achieve strategic goals.
             </p>
           </div>
           <ul className="skills-list">
             <li>
-              Custom software solutions for data analysis, visualization, and AI-powered tools.
+              Custom software solutions for data analysis, visualization and AI-powered tools.
             </li>
             <li>
-              Data strategy development, technology identification, and data workflow optimization.
+              Data strategy development, technology identification and data workflow optimization.
             </li>
             <li>
-              Data processing expertise including cleaning, organizing, and extracting insights.
+              Data processing expertise including cleaning, organizing and extracting insights.
             </li>
             <li>
               Machine learning API integration for web portals to enhance user experiences and
@@ -91,6 +112,23 @@ const About = () => {
             </li>
           </ul>
         </StyledText>
+        <StyledIndustries>
+          <h3>Industries</h3>
+          <ul>
+            <li>FMCG</li>
+            <li>Media</li>
+            <li>E-commerce</li>
+            <li>Manufacturing</li>
+            <li>Construction</li>
+          </ul>
+          <h3>Geographies</h3>
+          <ul>
+            <li>USA</li>
+            <li>UK</li>
+            <li>France</li>
+            <li>Poland</li>
+          </ul>
+        </StyledIndustries>
       </div>
     </StyledAboutSection>
   );
